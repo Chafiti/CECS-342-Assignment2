de7f46705c7c3959df18717920c8c504d619d264
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2023-10-16 23:53:20 -0700

    Creating coding files for the assignment

---
+++ sort.cpp
@@ -0,0 +1,7 @@
+/*
+  Writing a sorting algorithm that can handle any data type 
+  Then test the algorithm with test cases
+    - sort numbers ascending by numerical value,
+    - sort people alphabetically (lexicographically) by name, and to
+    - sort people descending by age, where people of the same age should be sorted alphabetically (lexicographically).
+*/
